12306eb76e19223b481ff2619602bd5fad0cfa17
suvrat128 <92354425+suvrat128@users.noreply.github.com>
2024-07-21 17:27:47 +0530

    Create 03_braking_innr_outer_val.py

---
+++ while_inside_while.py/03_braking_innr_outer_val.py
@@ -0,0 +1,65 @@
+#break inner loop with outer loop value
+
+i=1
+while i<6:
+    j=1
+    while j<6:
+        print(i,j)
+        if i==3:
+            break
+        j+=1
+    i+=1
+
+#process
+'''
+iteration 1:
+    outer loop i = 1:
+        inner loop j = 1:
+            so print 1,1
+            check i == 3:false
+            increment j from 1 to 2
+        inner loop j =2:
+            so print 1,2
+            check i == 3: false
+            increment j form 2 to 3
+        inner loop j =3:
+            so print 1,3
+            check i == 3: false
+            increment j from 3 to 4
+        inner loop j = 4:
+            so print 1,4
+            check i == 3: fasle
+            increment j from 4 to 5
+        inner loop j = 5:
+            so print 1,5
+            check i == 3: false
+            increment j from 5 to 6
+        in last increment  i from 1 to 2
+iteration 2:
+    outer loop i = 2:
+        inner loop will again run for 5 times :
+            so print 2,1 2,2 2,3 2,4 2,5
+            becuse in this iteration i == 3 will never true
+        in last increment i from 2 to 3
+
+iteration 3:
+    outer loop i =3 :
+        inner loop j = 1
+            so print 3,1
+            checkin i ==3 true
+            so terminate this inner loop
+        in last increment i from 3 to 4
+iteration 4:
+    outer loop i = 4:
+        inner loop will again run for 5 times :
+            so print 4,1 4,2 4,3 4,4 4,5
+            becuse in this iteration i == 3 will never true
+        in last increment i from 4 to 5
+iteration 5:
+    outer loop i = 5:
+        inner loop will again run for 5 times :
+            so print 5,1 5,2 5,3 5,4 5,5
+            becuse in this iteration i == 3 will never true
+        in last increment i from 5 to 6
+
+'''
