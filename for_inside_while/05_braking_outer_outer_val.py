bfb6e01e2032efcddfb74a9d1548a3543e34fa02
suvrat128 <92354425+suvrat128@users.noreply.github.com>
2024-07-21 17:13:12 +0530

    Update 05_braking_outer_outer_val.py

--- for_inside_while/05_braking_outer_outer_val.py
+++ for_inside_while/05_braking_outer_outer_val.py
@@ -7,3 +7,21 @@ while i<6:
     if j == 5:
         break
     i+=1
+
+#process:
+'''
+iteration 1:
+    outer loop i = 1:
+        inner loop j = 1:
+            so print 1,1
+        inner loop j = 2:
+            so print 1,2
+        inner loop j =3:
+            so print 1,3
+        inner loop j =4
+            so print 1,4
+        inner loop j =5
+            so print 1,5
+        check j == 5: true 
+        so terminated the outer loop so program will get end
+'''
