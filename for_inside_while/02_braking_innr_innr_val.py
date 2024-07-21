58ca99fa4be1cea557181043213da36dcc59d78f
suvrat128 <92354425+suvrat128@users.noreply.github.com>
2024-07-21 17:20:44 +0530

    Update 02_braking_innr_innr_val.py

--- for_inside_while/02_braking_innr_innr_val.py
+++ for_inside_while/02_braking_innr_innr_val.py
@@ -22,30 +22,30 @@ iteration 1:
             so print 1,3
             check 3 == 3: true 
                 so terminate the inner loop
-        increment i from 1 to 2
+        at last increment i from 1 to 2
 iteration 2:
     outer loop i = 2:
         inner loop will run againg till j == 3
             so print 2,1 2,2 2,3 
             after that j is 3 so inner loop will terminted
-        increment i from 2 to 3
+        at last increment i from 2 to 3
 iteration 3:
     outer loop i = 3:
         inner loop will run againg till j == 3
             so print 3,1 3,2 3,3 
             after that j is 3 so inner loop will terminted
-        increment i from 3 to 4
+        at last increment i from 3 to 4
 iteration 4:
     outer loop i = 4:
         inner loop will run againg till j == 3
             so print 4,1 4,2 4,3 
             after that j is 4 so inner loop will terminted
-        incremrnt i froom 4 to 5
+        at last incremrnt i froom 4 to 5
 iteration 5:
     outer loop i = 5:
         inner loop will run againg till j == 3
             so print 5,1 5,2 5,3 
             after that j is 3 so inner loop will terminted
-        incremrnt i from 5 to 6
+        at last incremrnt i from 5 to 6
 
 '''
