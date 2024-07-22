0323c7f674dc79a707c241f24fff2d019e520065
suvrat128 <92354425+suvrat128@users.noreply.github.com>
2024-07-22 19:04:59 +0530

    Update perfect_no.py

--- z-ques/perfect_no.py
+++ z-ques/perfect_no.py
@@ -6,17 +6,69 @@ outer loop : for -range ----> ll to ul
 inner loop : for range -----> 1 to n//2+1
 2. find out the sum of divisor and compare it with number
 
-'''
+
 
 ll = int(input())
 ul = int(input())
 for n in range(ll,ul+1):
     summ=0
     for i in range(1,n//2+1):
-        if n%1 == 0:
+        if n%i == 0:
             summ+=i
     if summ == n:
         print(n)
-
+'''
 # printing first n prefecrt number by using infinite loop
+'''
+count= int(input('enter how many prefect number u want:'))
+n=1
+c=0
 
+while True:
+    summ = 0
+    for i in range(1,n//2+1):
+        if n%i == 0:
+            summ+=i
+    if summ == n:
+        print(n)
+        c+=1
+        if count == c:
+            break
+    n+=1
+'''
+# print nth prefect no
+'''
+count= int(input('enter how many prefect number u want:'))
+n=1
+c=0
+
+while True:
+    summ = 0
+    for i in range(1,n//2+1):
+        if n%i == 0:
+            summ+=i
+    if summ == n:
+        
+        c+=1
+        if count == c:
+            print(n)
+            break
+    n+=1
+'''
+#print the 5th and 10th prefect no
+
+n=1
+c=0
+
+while True:
+    summ = 0
+    for i in range(1,n//2+1):
+        if n%i == 0:
+            summ+=i
+    if summ == n:
+        c+=1
+        if c>=1 and c<=3:
+            print(n)
+        if c==10:
+            break
+    n+=1
