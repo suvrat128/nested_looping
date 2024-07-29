645e5f0fe0a2994302b1b1b1eb7314c7e4af961b
suvrat128 <92354425+suvrat128@users.noreply.github.com>
2024-07-29 22:06:57 +0530

    Update special_no.py

--- z-ques/special_no.py
+++ z-ques/special_no.py
@@ -1,3 +1,7 @@
+# check wheather given no is spacial no or not
+'''
+A number is considered a special number if the sum of the factorial of the digits in the number is equal to the number itself.
+'''
 
 n = int(input())
 dummy = n
@@ -13,4 +17,26 @@ if summ == n:
     print('special no')
 else:
     print('not special no')
+
+
+# pint all special number in given rang
+
+ll = int(input())
+ul = int(input())
+
+for n in range(ll,ul+1):
+    dummy = n
+    summ = 0
+    while dummy>0:
+        rem = dummy%10
+        dummy//=10
+        fact = 1
+        for i in range(1,rem+1):
+            fact*=i
+        summ+=fact
+    if summ == n:
+        print(n)
+
+
+
 
