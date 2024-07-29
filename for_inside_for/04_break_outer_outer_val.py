877ca71b4681dd4dc44138032d60f53c9a32a68b
suvrat128 <92354425+suvrat128@users.noreply.github.com>
2024-07-29 21:59:42 +0530

    Update 04_break_outer_outer_val.py

--- for_inside_for/04_break_outer_outer_val.py
+++ for_inside_for/04_break_outer_outer_val.py
@@ -1,14 +1,15 @@
 #breaking outer loop with outer loop value
 #ex1
-'''
+
 for i in range(1,6):
     for j in range(1,6):
         print(i,j)
     if i == 3:
         break
-    '''
+    
 
 #ex2
+
 for i in range(1,6):
     if i == 3:
         break
